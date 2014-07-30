a4ddfe81786bf2e2d7bc9cffa465436d5fc93efe
dstepien <kontakt@dawidstepien.pl>
2014-07-30 17:59:05 +0200

    Initial commit

---
+++ src/main/java/pl/dawidstepien/App.java
@@ -0,0 +1,13 @@
+package pl.dawidstepien;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
